7a0aee1ee18c54d796ce02a9530d1942e2638d4c
Marcos Felipe <9345209@labsoft09.labsoft.local>
2017-11-30 18:16:34 -0200

    Classe de conexao com bdd

---
+++ RSAD/LojaDeComputadores/src/connection/ConexaoMySQL.java
@@ -0,0 +1,32 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoMySQL {
+
+	public static Connection getConexaoMySQL() {
+
+		Connection connection = null;
+		try {
+
+			String driverName = "com.mysql.jdbc.Driver";
+
+			Class.forName(driverName);
+
+			String username = "t1g2";
+			String password = "$wS!hMY%";
+
+			connection = DriverManager.getConnection("jdbc:mysql://143.107.102.5:3306/t1g2", username, password);
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return connection;
+
+	}
+
+}
