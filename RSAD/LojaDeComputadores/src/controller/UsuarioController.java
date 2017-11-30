2a5e48b4c02bbc69cadd640953c01e1e4358f82d
Daniel Shinji Hoshi <daniel.hoshi@usp.br>
2017-11-30 17:05:14 -0200

    pequena correção de conflito

--- RSAD/LojaDeComputadores/src/controller/UsuarioController.java
+++ RSAD/LojaDeComputadores/src/controller/UsuarioController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,20 +9,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import model.Usuario;
 import test.DadosTeste;
 
 /**
  * Servlet implementation class UsuarioController
  */
-@WebServlet("/SelecionarUsuarioController")
-public class SelecionarUsuarioController extends HttpServlet {
+@WebServlet("/UsuarioController")
+public class UsuarioController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
-	public SelecionarUsuarioController() {
+	public UsuarioController() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
@@ -34,18 +32,8 @@ public class SelecionarUsuarioController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		Integer idUsuario = Integer.parseInt(request.getParameter("id"));
-		//TODO selecionar do bd
-		Usuario usuario;
-		for (Usuario u : (ArrayList<Usuario>) request.getSession().getAttribute("listaUsuarios")){
-			if(u.getId() == idUsuario){
-				request.setAttribute("usuarioSelecionado", u);
-			}
-		}
-		request.setAttribute("listaCargos", DadosTeste.listaCargos());
-		request.setAttribute("listaTipos", DadosTeste.listaTipos());
-		
-		RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/usuario.jsp");
+		request.getSession().setAttribute("listaUsuarios", DadosTeste.newUsuarios());
+		RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/lista-usuario.jsp");
 		requestDispatcher.forward(request, response);
 	}
 
